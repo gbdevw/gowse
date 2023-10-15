26640d0d28d1f195689522a85383fdca25aa02ae
Guillaume B. <50517783+gbdevw@users.noreply.github.com>
2023-10-15 15:40:54 +0000

    Listen on all interfaces and on port 8081

--- example/server/providers/provide_echoserver.go
+++ example/server/providers/provide_echoserver.go
@@ -2,6 +2,7 @@ package providers
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gbdevw/gowsclient/echowsserver"
 	"go.uber.org/fx"
@@ -9,8 +10,8 @@ import (
 )
 
 func ProvideEchoServer(lc fx.Lifecycle, logger *zap.Logger) *echowsserver.EchoWebsocketServer {
-	// Build server - will listen on localhost:8080
-	srv := echowsserver.NewEchoWebsocketServer(nil, zap.NewStdLog(logger))
+	// Build server - will listen on all interfaces on port 8081
+	srv := echowsserver.NewEchoWebsocketServer(&http.Server{Addr: "0.0.0.0:8081"}, zap.NewStdLog(logger))
 	// Register Start and Stop hooks to Start and Stop the server
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
